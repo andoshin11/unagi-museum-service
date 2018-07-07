43f817fa9808942e7650a4130231b5a05c2532e2
andoshin11 <shinglish11@gmail.com>
2018-07-07 17:35:32 +0900

    Add musums api endpoint

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
+	"github.com/andoshin11/euphro-ddd/src/delivery/api/handler"
+	"github.com/andoshin11/euphro-ddd/src/gateway"
+	"github.com/andoshin11/euphro-ddd/src/repository"
+	"github.com/andoshin11/euphro-ddd/src/usecase"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,10 +19,24 @@ func test(c *gin.Context) {
 func main() {
 	router := gin.Default()
 
+	ctx := context.Background()
+	client, err := gateway.NewFirestoreClient(ctx)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	museumRepository := repository.NewMuseumRepository(client)
+	museumUseCase := usecase.NewMuseumUsecase(museumRepository)
+
+	// handlers
+	museumHandler := handler.NewMuseumHandler(museumUseCase)
+
 	// API namespace
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/test", test)
+		v1.GET("/museums", museumHandler.GetAll)
 	}
 
 	router.Run(":8080")
