7a9cff6783094d6dde705d31518646fb79683316
andoshin11 <shinglish11@gmail.com>
2018-07-07 17:32:07 +0900

    Add museum entity

---
+++ src/entity/museum.go
@@ -0,0 +1,10 @@
+package entity
+
+// Museum type definition
+type Museum struct {
+	ID        string  `json:"id"`
+	Address   string  `json:"address"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Name      string  `json:"name"`
+}
